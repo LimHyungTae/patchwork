a792c39343f26314bb7900abc102fd82518d3933
Hyungtae Lim <shapelim@kaist.ac.kr>
2021-09-19 07:56:12 +0900

    Refactoring for readability

--- include/patchwork/patchwork.hpp
+++ include/patchwork/patchwork.hpp
@@ -76,32 +76,17 @@ public:
         // CZM denotes 'Concentric Zone Model'. Please refer to our paper
         node_handle_.getParam("/patchwork/czm/num_zones", num_zones_);
         node_handle_.getParam("/patchwork/czm/num_sectors_each_zone", num_sectors_each_zone_);
-        node_handle_.getParam("/patchwork/czm/mum_rings_each_zone", num_rings_each_zone_);
+        node_handle_.getParam("/patchwork/czm/num_rings_each_zone", num_rings_each_zone_);
+        node_handle_.getParam("/patchwork/czm/min_ranges_each_zone", min_ranges_);
         node_handle_.getParam("/patchwork/czm/elevation_thresholds", elevation_thr_);
         node_handle_.getParam("/patchwork/czm/flatness_thresholds", flatness_thr_);
 
         ROS_INFO("Num. zones: %d", num_zones_);
 
-        if (num_zones_ != 4 || num_sectors_each_zone_.size() != num_rings_each_zone_.size()) {
-            throw invalid_argument("Some parameters are wrong! Check the num_zones and num_rings/sectors_each_zone");
-        }
-        if (elevation_thr_.size() != flatness_thr_.size()) {
-            throw invalid_argument("Some parameters are wrong! Check the elevation/flatness_thresholds");
-        }
+        check_input_parameters_are_correct();
+//        cout_params();
 
-        cout << (boost::format("Num. sectors: %d, %d, %d, %d") % num_sectors_each_zone_[0] % num_sectors_each_zone_[1] %
-                 num_sectors_each_zone_[2] %
-                 num_sectors_each_zone_[3]).str() << endl;
-        cout << (boost::format("Num. rings: %01d, %01d, %01d, %01d") % num_rings_each_zone_[0] %
-                 num_rings_each_zone_[1] %
-                 num_rings_each_zone_[2] %
-                 num_rings_each_zone_[3]).str() << endl;
-        cout << (boost::format("elevation_thr_: %0.4f, %0.4f, %0.4f, %0.4f ") % elevation_thr_[0] % elevation_thr_[1] %
-                 elevation_thr_[2] %
-                 elevation_thr_[3]).str() << endl;
-        cout << (boost::format("flatness_thr_: %0.4f, %0.4f, %0.4f, %0.4f ") % flatness_thr_[0] % flatness_thr_[1] %
-                 flatness_thr_[2] %
-                 flatness_thr_[3]).str() << endl;
+        // It equals to elevation_thr_.size()/flatness_thr_.size();
         num_rings_of_interest_ = elevation_thr_.size();
 
         node_handle_.param("/patchwork/visualize", visualize_, true);
@@ -118,16 +103,16 @@ public:
         revert_pc_pub = node_handle_.advertise<sensor_msgs::PointCloud2>("/revert_pc", 100);
         reject_pc_pub = node_handle_.advertise<sensor_msgs::PointCloud2>("/reject_pc", 100);
 
-        min_range_z2_ = (7 * min_range_ + max_range_) / 8.0;
-        min_range_z3_ = (3 * min_range_ + max_range_) / 4.0;
-        min_range_z4_ = (min_range_ + max_range_) / 2.0;
+        min_range_z2_ = min_ranges_[1];
+        min_range_z3_ = min_ranges_[2];
+        min_range_z4_ = min_ranges_[3];
 
-        min_ranges = {min_range_, min_range_z2_, min_range_z3_, min_range_z4_};
-        ring_sizes = {(min_range_z2_ - min_range_) / num_rings_each_zone_.at(0),
+        min_ranges_ = {min_range_, min_range_z2_, min_range_z3_, min_range_z4_};
+        ring_sizes_ = {(min_range_z2_ - min_range_) / num_rings_each_zone_.at(0),
                       (min_range_z3_ - min_range_z2_) / num_rings_each_zone_.at(1),
                       (min_range_z4_ - min_range_z3_) / num_rings_each_zone_.at(2),
                       (max_range_ - min_range_z4_) / num_rings_each_zone_.at(3)};
-        sector_sizes = {2 * M_PI / num_sectors_each_zone_.at(0), 2 * M_PI / num_sectors_each_zone_.at(1),
+        sector_sizes_ = {2 * M_PI / num_sectors_each_zone_.at(0), 2 * M_PI / num_sectors_each_zone_.at(1),
                         2 * M_PI / num_sectors_each_zone_.at(2),
                         2 * M_PI / num_sectors_each_zone_.at(3)};
         cout << "INITIALIZATION COMPLETE" << endl;
@@ -185,9 +170,9 @@ private:
     vector<int> num_sectors_each_zone_;
     vector<int> num_rings_each_zone_;
 
-    vector<double> sector_sizes;
-    vector<double> ring_sizes;
-    vector<double> min_ranges;
+    vector<double> sector_sizes_;
+    vector<double> ring_sizes_;
+    vector<double> min_ranges_;
     vector<double> elevation_thr_;
     vector<double> flatness_thr_;
 
@@ -202,6 +187,10 @@ private:
 
     pcl::PointCloud<PointT> regionwise_ground_;
     pcl::PointCloud<PointT> regionwise_nonground_;
+
+    void check_input_parameters_are_correct();
+
+    void cout_params();
 
     void initialize_zone(Zone &z, int num_sectors, int num_rings);
 
@@ -243,6 +232,7 @@ private:
             const double surface_variable);
 
 };
+
 
 template<typename PointT> inline
 void PatchWork<PointT>::initialize_zone(Zone &z, int num_sectors, int num_rings) {
@@ -334,6 +324,8 @@ void PatchWork<PointT>::estimate_ground(
         pcl::PointCloud<PointT> &cloud_out,
         pcl::PointCloud<PointT> &cloud_nonground,
         double &time_taken) {
+
+    // Just for visualization
     poly_list_.header.stamp = ros::Time::now();
     if (!poly_list_.polygons.empty()) poly_list_.polygons.clear();
     if (!poly_list_.likelihood.empty()) poly_list_.likelihood.clear();
@@ -509,20 +501,20 @@ void PatchWork<PointT>::pc2czm(const pcl::PointCloud<PointT> &src, std::vector<Z
             double theta = xy2theta(pt.x, pt.y);
 
             if (r < min_range_z2_) { // In First rings
-                ring_idx = min(static_cast<int>(((r - min_range_) / ring_sizes[0])), num_rings_each_zone_[0] - 1);
-                sector_idx = min(static_cast<int>((theta / sector_sizes[0])), num_sectors_each_zone_[0] - 1);
+                ring_idx = min(static_cast<int>(((r - min_range_) / ring_sizes_[0])), num_rings_each_zone_[0] - 1);
+                sector_idx = min(static_cast<int>((theta / sector_sizes_[0])), num_sectors_each_zone_[0] - 1);
                 czm[0][ring_idx][sector_idx].points.emplace_back(pt);
             } else if (r < min_range_z3_) {
-                ring_idx = min(static_cast<int>(((r - min_range_z2_) / ring_sizes[1])), num_rings_each_zone_[1] - 1);
-                sector_idx = min(static_cast<int>((theta / sector_sizes[1])), num_sectors_each_zone_[1] - 1);
+                ring_idx = min(static_cast<int>(((r - min_range_z2_) / ring_sizes_[1])), num_rings_each_zone_[1] - 1);
+                sector_idx = min(static_cast<int>((theta / sector_sizes_[1])), num_sectors_each_zone_[1] - 1);
                 czm[1][ring_idx][sector_idx].points.emplace_back(pt);
             } else if (r < min_range_z4_) {
-                ring_idx = min(static_cast<int>(((r - min_range_z3_) / ring_sizes[2])), num_rings_each_zone_[2] - 1);
-                sector_idx = min(static_cast<int>((theta / sector_sizes[2])), num_sectors_each_zone_[2] - 1);
+                ring_idx = min(static_cast<int>(((r - min_range_z3_) / ring_sizes_[2])), num_rings_each_zone_[2] - 1);
+                sector_idx = min(static_cast<int>((theta / sector_sizes_[2])), num_sectors_each_zone_[2] - 1);
                 czm[2][ring_idx][sector_idx].points.emplace_back(pt);
             } else { // Far!
-                ring_idx = min(static_cast<int>(((r - min_range_z4_) / ring_sizes[3])), num_rings_each_zone_[3] - 1);
-                sector_idx = min(static_cast<int>((theta / sector_sizes[3])), num_sectors_each_zone_[3] - 1);
+                ring_idx = min(static_cast<int>(((r - min_range_z4_) / ring_sizes_[3])), num_rings_each_zone_[3] - 1);
+                sector_idx = min(static_cast<int>((theta / sector_sizes_[3])), num_sectors_each_zone_[3] - 1);
                 czm[3][ring_idx][sector_idx].points.emplace_back(pt);
             }
         }
@@ -630,16 +622,16 @@ geometry_msgs::PolygonStamped PatchWork<PointT>::set_polygons(int zone_idx, int 
     geometry_msgs::Point32 point;
 
     // RL
-    double zone_min_range = min_ranges[zone_idx];
-    double r_len = r_idx * ring_sizes[zone_idx] + zone_min_range;
-    double angle = theta_idx * sector_sizes[zone_idx];
+    double zone_min_range = min_ranges_[zone_idx];
+    double r_len = r_idx * ring_sizes_[zone_idx] + zone_min_range;
+    double angle = theta_idx * sector_sizes_[zone_idx];
 
     point.x = r_len * cos(angle);
     point.y = r_len * sin(angle);
     point.z = MARKER_Z_VALUE;
     polygons.polygon.points.push_back(point);
     // RU
-    r_len = r_len + ring_sizes[zone_idx];
+    r_len = r_len + ring_sizes_[zone_idx];
     point.x = r_len * cos(angle);
     point.y = r_len * sin(angle);
     point.z = MARKER_Z_VALUE;
@@ -647,21 +639,21 @@ geometry_msgs::PolygonStamped PatchWork<PointT>::set_polygons(int zone_idx, int 
 
     // RU -> LU
     for (int idx = 1; idx <= num_split; ++idx) {
-        angle = angle + sector_sizes[zone_idx] / num_split;
+        angle = angle + sector_sizes_[zone_idx] / num_split;
         point.x = r_len * cos(angle);
         point.y = r_len * sin(angle);
         point.z = MARKER_Z_VALUE;
         polygons.polygon.points.push_back(point);
     }
 
-    r_len = r_len - ring_sizes[zone_idx];
+    r_len = r_len - ring_sizes_[zone_idx];
     point.x = r_len * cos(angle);
     point.y = r_len * sin(angle);
     point.z = MARKER_Z_VALUE;
     polygons.polygon.points.push_back(point);
 
     for (int idx = 1; idx < num_split; ++idx) {
-        angle = angle - sector_sizes[zone_idx] / num_split;
+        angle = angle - sector_sizes_[zone_idx] / num_split;
         point.x = r_len * cos(angle);
         point.y = r_len * sin(angle);
         point.z = MARKER_Z_VALUE;
@@ -695,6 +687,54 @@ void PatchWork<PointT>::set_ground_likelihood_estimation_status(
     } else { // tilted
         poly_list_.likelihood.push_back(TOO_TILTED);
     }
+}
+
+
+template<typename PointT> inline
+void PatchWork<PointT>::check_input_parameters_are_correct(){
+    string SET_SAME_SIZES_OF_PARAMETERS = "Some parameters are wrong! the size of parameters should be same";
+
+    int n_z = num_zones_;
+    int n_r = num_rings_each_zone_.size();
+    int n_s = num_sectors_each_zone_.size();
+    int n_m = min_ranges_.size();
+
+    if ((n_z != n_r) || (n_z != n_s) || (n_z != n_m)) {
+        throw invalid_argument(SET_SAME_SIZES_OF_PARAMETERS);
+    }
+
+    if ((n_r != n_s) || (n_r != n_m) || (n_s != n_m)) {
+        throw invalid_argument(SET_SAME_SIZES_OF_PARAMETERS);
+    }
+
+    if (min_range_ != min_ranges_[0]) {
+        throw invalid_argument("Setting min. ranges are weired! The first term should be eqaul to min_range_");
+    }
+
+    if (elevation_thr_.size() != flatness_thr_.size()) {
+        throw invalid_argument("Some parameters are wrong! Check the elevation/flatness_thresholds");
+    }
+
+}
+
+
+template<typename PointT> inline
+void PatchWork<PointT>::cout_params(){
+    cout << (boost::format("Num. sectors: %d, %d, %d, %d") % num_sectors_each_zone_[0] % num_sectors_each_zone_[1] %
+             num_sectors_each_zone_[2] %
+             num_sectors_each_zone_[3]).str() << endl;
+    cout << (boost::format("Num. rings: %01d, %01d, %01d, %01d") % num_rings_each_zone_[0] %
+             num_rings_each_zone_[1] %
+             num_rings_each_zone_[2] %
+             num_rings_each_zone_[3]).str() << endl;
+    cout << (boost::format("elevation_thr_: %0.4f, %0.4f, %0.4f, %0.4f ") % elevation_thr_[0] % elevation_thr_[1] %
+             elevation_thr_[2] %
+             elevation_thr_[3]).str() << endl;
+    cout << (boost::format("flatness_thr_: %0.4f, %0.4f, %0.4f, %0.4f ") % flatness_thr_[0] % flatness_thr_[1] %
+             flatness_thr_[2] %
+             flatness_thr_[3]).str() << endl;
+
+
 }
 
 #endif
